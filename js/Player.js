a3dff18064e4d6261bfca7265ab2ce404049c02c
Shradha <rajan.bhatia@hotmail.com>
2020-11-23 15:55:13 +0530

    real car racing

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank=null;
   }
 
   getCount(){
@@ -25,7 +26,20 @@ class Player {
       distance:this.distance
     });
   }
+  getRank(){
+var rankRef=database.ref('rank');
+rankRef.on("value",(data)=>{
+  this.rank=data.val();
+})
 
+
+}
+ static updateRank(rank){
+  database.ref("/").update({
+    carsATEnd:rank
+
+  });
+}
   static getPlayerInfo(){
     var playerInfoRef = database.ref('players');
     playerInfoRef.on("value",(data)=>{
